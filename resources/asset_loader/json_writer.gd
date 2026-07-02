5b5d34fe867b41864cdabccd1ebc61e5acc473e2
Schifus <Alfredcastaldi@proton.me>
2026-07-02 19:01:19 +0200

    asset_loader 1.1

--- resources/asset_loader/json_writer.gd
+++ resources/asset_loader/json_writer.gd
@@ -22,11 +22,15 @@ func is_folder_valid(file_path: String):
 func _ready():
 	# Iniziamo la scansione dalla root, tutto qui.
 	test_scan(ROOT)
+	print(scanned_folders)
 	print(path_dictionary)
 
-
+var scanned_folders = []
 func test_scan(path: String):
-	
+	var flag = false
+	if (scanned_folders.has(path.trim_suffix("/").get_file())):
+		print("CE LAAAAA")
+		flag = true
 	var pointer = DirAccess.open(path)
 	if not pointer: return
 	pointer.list_dir_begin()
@@ -45,10 +49,29 @@ func test_scan(path: String):
 					file_name = pointer.get_next()
 					continue
 				else:
-					if(!path_dictionary.has(path)):
-						path_dictionary[path] = []
-						path_dictionary[path].append(file_name)
+					
+					var key = get_dictionary_key_from_dir(path)
+					if (flag):
+						key = update_existing_key(key)
+						path_dictionary[key] = []
+						path_dictionary[key].append(file_name)
+						scanned_folders.append(key)
+						
+					if(!path_dictionary.has(key)):
+						
+						path_dictionary[key] = []
+						path_dictionary[key].append(file_name)
+						scanned_folders.append(path.trim_suffix("/").get_file())
 					else:
 						
-						path_dictionary[path].append(file_name)
+						path_dictionary[key].append(file_name)
 			file_name = pointer.get_next()
+	
+
+
+func get_dictionary_key_from_dir(path):
+	return path.trim_suffix("/").get_file()
+	
+
+func update_existing_key(key):
+	return key.path_join(str(scanned_folders.count(key)))
