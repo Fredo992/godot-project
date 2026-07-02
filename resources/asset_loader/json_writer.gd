15a4b5a7eaaf7d73ca37ab6355634f05315cb4ac
Schifus <Alfredcastaldi@proton.me>
2026-07-02 19:36:14 +0200

    asset loader 1.3

--- resources/asset_loader/json_writer.gd
+++ resources/asset_loader/json_writer.gd
@@ -1,7 +1,5 @@
 extends Node
 
-
-# Questo è il tuo "Configuratore"
 const ASSET_CONFIG = {
 	"PNG": ".png",
 	"SCENE": ".tscn"
@@ -9,69 +7,55 @@ const ASSET_CONFIG = {
 
 const ROOT = "res://resources"
 
+var path_dictionary: Dictionary = {}
+var folder_counters: Dictionary = {}
 
-var path_dictionary: Dictionary[String, Array] = {}
-
-
-
-func is_folder_valid(file_path: String):
-	return ASSET_CONFIG.keys().has(file_path.get_extension())
-
-	
-	
 func _ready():
-	# Iniziamo la scansione dalla root, tutto qui.
 	test_scan(ROOT)
-	print(scanned_folders)
+	print("--- RISULTATO FINALE ---")
+	test_scan(ROOT)
 	print(path_dictionary)
 
-var scanned_folders = []
 func test_scan(path: String):
-	var flag = false
-	if (scanned_folders.has(path.trim_suffix("/").get_file())):
-		print("CE LAAAAA")
-		flag = true
 	var pointer = DirAccess.open(path)
-	if not pointer: return
+	if not pointer: 
+		return
+	
 	pointer.list_dir_begin()
+	
+	# 1. Determiniamo la chiave per la cartella corrente
+	var base_key = path.trim_suffix("/").get_file()
+	var final_key = generate_unique_key(base_key)
+	
+	# Inizializziamo il dizionario per questa chiave
+	if not path_dictionary.has(final_key):
+		path_dictionary[final_key] = []
+		
 	var file_name = pointer.get_next()
+	
+	# 2. Ciclo di scansione
 	while(file_name != ""):
 		if file_name != "." and file_name != "..":
 			var current_path = path.path_join(file_name)
-			if (file_name == "asset_loader"):
+			
+			# Filtri di esclusione
+			if file_name == "asset_loader" or file_name.ends_with(".import"):
 				file_name = pointer.get_next()
 				continue
 			
-			if(pointer.current_is_dir()):
+			# Ricorsione o inserimento
+			if pointer.current_is_dir():
 				test_scan(current_path)
 			else:
-				if(file_name.ends_with(".import")):
-					file_name = pointer.get_next()
-					continue
-				else:
-					
-					var key = get_dictionary_key_from_dir(path)
-					if (flag):
-						key = update_existing_key(key)
-						path_dictionary[key] = []
-						path_dictionary[key].append(file_name)
-						scanned_folders.append(key)
-						
-					if(!path_dictionary.has(key)):
-						
-						path_dictionary[key] = []
-						path_dictionary[key].append(file_name)
-						scanned_folders.append(path.trim_suffix("/").get_file())
-					else:
-						
-						path_dictionary[key].append(file_name)
-			file_name = pointer.get_next()
-	
+				path_dictionary[final_key].append(file_name)
+		
+		file_name = pointer.get_next()
 
-
-func get_dictionary_key_from_dir(path):
-	return path.trim_suffix("/").get_file()
-	
-
-func update_existing_key(key):
-	return key.path_join(str(scanned_folders.count(key)))
+func generate_unique_key(key: String) -> String:
+	# Se la chiave è già stata usata, ne creiamo una versione indicizzata
+	if not folder_counters.has(key):
+		folder_counters[key] = 0
+		return key
+	else:
+		folder_counters[key] += 1
+		return key + "_" + str(folder_counters[key])
